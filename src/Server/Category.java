6e0470ababbb715f77b321489ed112523cdab789
Christer Bengtsson <mail@christerbengtsson.se>
2024-11-25 10:55:05 +0100

    copied Charlies old code and modified it to randomize categories.

--- src/Server/Category.java
+++ src/Server/Category.java
@@ -1,5 +1,9 @@
 package Server;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public enum Category {
     HISTORY,
     SCIENCE,
@@ -14,5 +18,18 @@ public enum Category {
             System.err.println("Invalid category: " + category);
             return HISTORY; // Default category
         }
+    }
+
+    public static List<Category> randomCategories() {
+
+        List<Category> categoryList = new ArrayList<>(List.of(Category.values()));
+
+        Collections.shuffle(categoryList);
+
+        List<Category> selectedCategories = new ArrayList<>(categoryList.subList(0, 4));
+
+
+
+      return selectedCategories;
     }
 }
