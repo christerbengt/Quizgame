7ef2a0b4cbcd3637adeb525080ab4a2092396458
Christer Bengtsson <mail@christerbengtsson.se>
2024-11-26 17:48:44 +0100

    Exchanged Localhost for actual IP in QuizClient

--- src/Client/QuizClient.java
+++ src/Client/QuizClient.java
@@ -12,7 +12,7 @@ import java.util.*;
 import java.util.List;
 
 public class QuizClient {
-    private final String SERVER_ADDRESS = "localhost";
+    private final String SERVER_ADDRESS = "192.168.68.61"; // Christers IPv4
     private final int SERVER_PORT = 12345;
     private ObjectOutputStream out;
     private ObjectInputStream in;
